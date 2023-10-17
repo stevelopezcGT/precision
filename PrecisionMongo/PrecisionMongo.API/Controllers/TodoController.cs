6dd86b9775770d764afe140d0875f1893c2b58eb
Victor Steve Lopez Camey <Victor.lc@alliedglobal.com>
2023-10-17 13:32:29 -0600

    Adding Open API documentation

--- PrecisionMongo/PrecisionMongo.API/Controllers/TodoController.cs
+++ PrecisionMongo/PrecisionMongo.API/Controllers/TodoController.cs
@@ -6,6 +6,9 @@ using PrecisionMongo.Core.Interfaces;
 
 namespace PrecisionMongo.API.Controllers
 {
+    /// <summary>
+    /// Todo controller
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class TodoController : ControllerBase
@@ -17,21 +20,37 @@ namespace PrecisionMongo.API.Controllers
             this.todoService = todoService;
         }
 
+        /// <summary>
+        /// Get All Todo items
+        /// </summary>
+        /// <returns>Todo's list</returns>
         [HttpGet]
-
         public async Task<List<TodoDTO>> GetAll()
         {
             return await todoService.GetAll();
         }
 
+        /// <summary>
+        /// Get Todo items with pagination
+        /// </summary>
+        /// <param name="pageSize">Size for every page returned</param>
+        /// <param name="pageNumber">Page number</param>
+        /// <returns>A Todo's list with into an instance of <see cref="ItemPaginationResponseDTO"/></returns>
         [HttpGet("WithPagination")]
-
         public async Task<ItemPaginationResponseDTO> GetAllWithPagination(int pageSize, int pageNumber)
         {
             return await todoService.GetAll(pageSize, pageNumber);
         }
 
+        /// <summary>
+        /// Update Todo item
+        /// </summary>
+        /// <param name="id">Id for item to update</param>
+        /// <param name="todoDTO">Todo instance to update</param>
+        /// <returns>No returns</returns>
         [HttpPut("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
         public async Task<IActionResult> PutTodoDTO(string id, TodoDTO todoDTO)
         {
             if (id != todoDTO.Id)
@@ -41,23 +60,34 @@ namespace PrecisionMongo.API.Controllers
 
             await todoService.UpdateAsync(id, todoDTO);
 
-            return NoContent();
+            return Ok();
         }
 
-
+        /// <summary>
+        /// Create Todo item
+        /// </summary>
+        /// <param name="todoCreateDTO">Todo instance to be created</param>
+        /// <returns>No returns</returns>
         [HttpPost]
+        [ProducesResponseType(200)]
         public async Task<ActionResult<bool>> PostTodoCreateDTO(TodoCreateDTO todoCreateDTO)
         {
             await todoService.CreateAsync(todoCreateDTO);
-            return NoContent();
+            return Ok();
         }
 
+        /// <summary>
+        /// Delete Todo item
+        /// </summary>
+        /// <param name="id">Todo Id to be deleted</param>
+        /// <returns></returns>
         [HttpDelete("{id}")]
+        [ProducesResponseType(200)]
         public async Task<IActionResult> DeleteTodo(string id)
         {
             await todoService.RemoveAsync(id);
 
-            return NoContent();
+            return Ok();
         }
 
     }
